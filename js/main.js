a6de290bbdac108ce8148c307f7e3c684d0bacb8
Well <wellingtonronaldodasilva@outlook.com>
2023-04-27 00:32:11 -0300

    Meu Site Pessoal - Empreender

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick ({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    SlidesToShow: 1,
+    adaptiveheight: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+});
